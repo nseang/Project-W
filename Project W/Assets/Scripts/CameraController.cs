521e934e9cb8a78f6d59437598695a454cbfd76c
Narith Seang <nseang2@gmail.com>
2018-04-21 18:36:28 -0400

    inital Project

---
+++ Project W/Assets/Scripts/CameraController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+    private GameObject player;
+    //private Control playerScript;
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private Vector3 offsetR;
+
+    private float smoothSpeed = 4f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+       // playerScript = (Control)player.GetComponent(typeof(Control));
+        target = player.transform;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        moveCamera();
+    }
+
+    private void moveCamera()
+    {
+
+       // bool facingRight = playerScript.getFacingRight();
+
+
+
+
+        Vector3 desiredposition;
+
+        desiredposition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredposition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        /*
+        if (facingRight)
+        {
+            desiredposition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredposition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+        }
+        if (!facingRight)
+        {
+            desiredposition = target.position + offsetR;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredposition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+        }
+
+        */
+
+
+
+    }
+
+}
---
+++ Project W/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float moveSpeed;
+
+
+    private Rigidbody myRigidBody;
+    private bool grounded;
+
+    private bool facingRight;
+
+    private bool jumpButton;
+
+	// Use this for initialization
+	void Start () {
+
+        myRigidBody = GetComponent<Rigidbody>();
+        facingRight = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+        HandleInput();
+        Flip(horizontal);
+	}
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Movement(horizontal);
+    }
+
+    private void HandleInput()
+    {
+        //Jump
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            jumpButton = true;
+        }
+    }
+
+    private void Movement(float horizontal)
+    {
+        //Horizontal Movement
+        myRigidBody.velocity = new Vector2(horizontal * moveSpeed, myRigidBody.velocity.y);
+
+
+
+        //Jump
+        if (grounded && jumpButton)
+        {
+            myRigidBody.AddForce(new Vector2(0, jumpForce));
+            jumpButton = false;
+        }
+    }
+
+    private void Flip(float horizontal)
+    {
+        Vector3 scale = transform.localScale;
+
+        if (horizontal < 0 && facingRight)
+        {
+            scale.x *= -1;
+            transform.localScale = scale;
+            facingRight = false;
+        }
+        if (horizontal > 0 && !facingRight)
+        {
+            scale.x *= -1;
+            transform.localScale = scale;
+            facingRight = true;
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if(other.tag == "Ground")
+        {
+            grounded = true;
+        }
+       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        grounded = false;
+    }
+}
